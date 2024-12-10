2391b641587f194ea82234c92ae8a21103ccfc71
unknown <alfinohasan629@gmail.com>
2024-12-10 17:35:06 +0700

    perubahanurl pada /data menjadi /, dan menambah route get yang berdasarkan id

--- routes/post.js
+++ routes/post.js
@@ -19,8 +19,42 @@ router.get("/", function (req, res) {
         return res.status(200).json({
           status: true,
           message: "Menampilkan data posts",
-          data: result, // mengembalikan data posts
+          data: result, //  mengembalikan data posts
         });
+      }
+    }
+  );
+});
+
+router.get("/data/:id", function (req, res) {
+  const { id } = req.params; // mengambil id dari permintaan user yang dari params id
+  // jika tidak ada id nya maka kembalikan error
+  if (!id) {
+    return res.status(403).json({
+      message: "Id Tidak Ada",
+    });
+  }
+  // jika id ada maka jalankan query
+  connection.query(
+    "SELECT * FROM posts WHERE id = ?",
+    [id],
+    function (error, results) {
+      if (error) {
+        return res.status(500).json({
+          status: false,
+          message: error.message,
+        });
+      } else {
+        if (results.length === 0) {
+          return res.status(404).json({
+            status: false,
+            message: "Post tidak ditemukan",
+          });
+        } else {
+          return res.status(200).json({
+            data: results[0], // Menampilkan hasil pertama
+          });
+        }
       }
     }
   );
