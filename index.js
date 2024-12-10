522779c508ddc6ff650dce78a5bfb6a85fc1e344
unknown <alfinohasan629@gmail.com>
2024-12-10 17:26:36 +0700

    import routes dan menggunakan semua route yang ada di folder routes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// import route
+const postsRouter = require("./routes/post");
 // inisialisasi express
 const app = express();
 
 // inisialisasi body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 // inisialisasi port
 const port = 3000;
@@ -16,6 +18,8 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Halo Cia cantik");
 });
+
+app.use("/api/posts", postsRouter);
 
 //  Digunakan untuk membuat server mendengarkan permintaan HTTP pada port tertentu.
 app.listen(port, () => {
